ce1358ae036ddf922ce6ca7456247cc2a1b7e77b
josedavid-celsor <josedavidrosalesrios0310@gmail.com>
2023-01-31 21:03:18 +0100

    Arreglada la factura

--- src/app/factura/factura.component.ts
+++ src/app/factura/factura.component.ts
@@ -1,3 +1,5 @@
+import { AuthService } from './../services/Auth.service';
+import { User } from './../interfaces/User';
 import { Component, inject, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { PageEvent, MatPaginator } from '@angular/material/paginator';
@@ -15,7 +17,8 @@ import { FacturaService } from '../services/Factura.service';
   styleUrls: ['./factura.component.scss']
 })
 export class FacturaComponent {
-  listadoFactura: Factura[]
+  listadoFactura: Factura[];
+  user: User;
   columnasTipo: string[] = ["ID", "Fecha", "Precio Total", "IVA"]
   modeloTipo: string[] = ["id", "fecha", "totalPrecio", "iva"]
   totalFacturas: number = 0
@@ -25,6 +28,7 @@ export class FacturaComponent {
   listTipo: Array<TipoProducto>
   facturaService: FacturaService = inject(FacturaService);
   compraService: CompraService = inject(CompraService);
+  authService: AuthService = inject(AuthService);
   accionesTabla: GT_Action[] = [new GT_Action("download", "dowload a new subtype", 'download')]
 
   /**
@@ -36,15 +40,20 @@ export class FacturaComponent {
 
   ngOnInit() {
     this.getFacturaPlist()
+    this.getUser();
   }
   getFacturaPlist() {
     this.facturaService.getFacturaPlist().subscribe(facturas => {
-      this.listadoFactura = facturas.content
-      this.totalFacturas = facturas.totalElements
+      this.listadoFactura = facturas.content;
+      this.totalFacturas = facturas.totalElements;
       if (this.tablaTipos) {
         this.tablaTipos.refreshData(this.listadoFactura)
       }
     })
+  }
+
+  getUser() {
+    this.user = this.authService.getUser();
   }
 
   notification(event: { action: string, entity: Factura }) {
@@ -69,34 +78,61 @@ export class FacturaComponent {
 
   public downloadAsPDF(factura: Factura) {
     this.compraService.getByFactura(factura).subscribe(compras => {
+      console.log(this.user)
 
       const doc = new jsPDF();
-      doc.text("Datos factura", 5, 10);
-      autoTable(doc, {
-        head: [['Fecha', 'Precio Total', 'IVA']],
-        body: [
-          [factura.fecha.toLocaleString(), factura.totalPrecio, factura.iva],
 
-          // ...
-        ],
-        tableId: 'Facturas',
-        margin: { left: 5 },
-        startY: 15
-      })
-      doc.text("Listado de productos", 5, 45);
+      const img = new Image();
+      img.src = "assets/img/logo.png"
+      doc.addImage(img, "png", 160, 7, 40, 20);
+
+      doc.setFontSize(35);
+      doc.setFont('helvetica', 'normal', 'bold');
+      doc.text("DATOS DE FACTURA", 15, 20);
+
+      doc.setFontSize(14);
+      doc.text("CLIENTE", 15, 35);
+      doc.text("FECHA:", 75, 35);
+
+      doc.setFontSize(10);
+      doc.setFont('helvetica', 'normal', 'normal');
+      doc.text(this.user.nombre + " " + this.user.apellido + " " + this.user.apellido2, 15, 42);
+      doc.text(this.user.email, 15, 47);
+
+      doc.setFontSize(11);
+      doc.text(factura.fecha.toLocaleString(), 95, 34.8);
+
       const bodyTabla = []
-      for(const compra of compras){
-        bodyTabla.push([compra.producto.nombre, compra.cantidad, compra.producto.precio, compra.cantidad*compra.producto.precio])
+      let subTotal = 0;
+      for (const compra of compras) {
+        bodyTabla.push([compra.producto.nombre, compra.cantidad, compra.producto.precio, compra.cantidad * compra.producto.precio])
+        subTotal += compra.cantidad * compra.producto.precio;
         console.log(bodyTabla)
       }
+      bodyTabla.push([' ', ' ', ' ', ' ']);
+      bodyTabla.push([' ', ' ', 'Subtotal', subTotal]);
+      bodyTabla.push([' ', ' ', 'IVA', factura.iva + " %"]);
+      bodyTabla.push([' ', ' ', 'Total', factura.totalPrecio + " €"]);
+      //console.log(subTotal);
       autoTable(doc, {
-        head: [['Nombre Producto', 'Cantidad', 'Precio', 'Total']],
+        head: [['Nombre Producto', 'Cantidad', 'Precio Unitario', 'Importe']],
+        headStyles: { fillColor: '#4054b4' },
         body: bodyTabla,
+        willDrawCell: (data) => {
+          let rows = data.table.body;
+          if (data.row.index === rows.length - 1) {
+            doc.setFont('helvetica', 'normal', 'bold');
+          }
+          if (data.row.index === rows.length - 1 || data.row.index === rows.length - 2 || data.row.index === rows.length - 3 || data.row.index === rows.length - 4) {
+            doc.setFillColor('white');
+          }
+        },
         tableId: 'Productos',
-        margin: { left: 5 },
-        startY: 50
+        margin: { left: 15 },
+        startY: 60,
+
       })
-      doc.save('table.pdf')
+      doc.save('DatosFactura.pdf')
     })
   }
 }
